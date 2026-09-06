3a105c16300af7acfa4166fb670dae8dd273d9fd
Venkatesh Prasad <venkateshprasaddr@gmail.com>
2026-09-06 12:55:07 +0530

    Initial Selenium Maven Jenkins project

---
+++ jenkins-selenium-demo/src/test/java/tests/GoogleTitleTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GoogleTitleTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        // Selenium Manager finds and configures the compatible Chrome driver.
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void verifyGoogleTitle() {
+        driver.get("https://www.google.com");
+
+        Assert.assertEquals(driver.getTitle(), "Google");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
