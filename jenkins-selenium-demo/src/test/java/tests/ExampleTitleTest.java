cc2444d544f47f3cdb3de184873c8ee774c8d778
Venkatesh Prasad <venkateshprasaddr@gmail.com>
2026-09-08 20:10:40 +0530

    Add second Selenium test and ignore Maven target

---
+++ jenkins-selenium-demo/src/test/java/tests/ExampleTitleTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ExampleTitleTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void verifyExampleTitle() {
+        driver.get("https://example.com");
+
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(actualTitle, "Example Domain");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
