b2fe962f984c326c23e2df70105115149d1981a7
Venkatesh Prasad <venkateshprasaddr@gmail.com>
2026-09-09 22:26:17 +0530

    new test case 4 added Wikipedia

---
+++ jenkins-selenium-demo/src/test/java/tests/WikipediaTitleTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class WikipediaTitleTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void verifyWikipediaTitle() {
+        driver.get("https://www.wikipedia.org/");
+
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(actualTitle, "Wikipedia");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
